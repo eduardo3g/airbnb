0d7977689a4a63c5b6f8b6e10b460d4420074498
eduardo3g <eduardosbrasil10@gmail.com>
2021-10-27 20:10:14 -0300

    style: create the Footer component

---
+++ components/Footer.js
@@ -0,0 +1,43 @@
+function Footer() {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600">
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">ABOUT</h5>
+        <p>How Airbnb works</p>
+        <p>Newsroom</p>
+        <p>Investors</p>
+        <p>Airbnb Plus</p>
+        <p>Airbnb Luxe</p>
+      </div>
+
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">COMMUNITY</h5>
+        <p>Accessibility</p>
+        <p>This is not a real site</p>
+        <p>It's a pretty awesome clone</p>
+        <p>Referrals accepted</p>
+        <p>Next.js all the way</p>
+      </div>
+
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">HOST</h5>
+        <p>Eduardo Santana</p>
+        <p>Presents</p>
+        <p>A perfect Airbnb clone</p>
+        <p>Check it out</p>
+        <p>Source code available on Github</p>
+      </div>
+
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">SUPPORT</h5>
+        <p>Help Center</p>
+        <p>Trust & Safety</p>
+        <p>Our COVID-19 Response</p>
+        <p>Neighborhood Support</p>
+        <p>Cancellation options</p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
