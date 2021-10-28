3db52fb2507522cd4409ae73b13d28c618d78c43
eduardo3g <eduardosbrasil10@gmail.com>
2021-10-28 11:37:54 -0300

    style: implement the Search component inside the header

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { useRouter } from "next/dist/client/router";
 import { useState } from "react";
 import {
   SearchIcon,
@@ -11,11 +12,12 @@ import "react-date-range/dist/styles.css";
 import "react-date-range/dist/theme/default.css";
 import { DateRangePicker } from "react-date-range";
 
-function Header() {
+function Header({ placeholder }) {
   const [searchInput, setSearchInput] = useState("");
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
   const [noOfGuests, setNoOfGuests] = useState(1);
+  const router = useRouter();
 
   const handleSelect = (ranges) => {
     setStartDate(ranges.selection.startDate);
@@ -34,10 +36,25 @@ function Header() {
     key: "selection",
   };
 
+  const search = () => {
+    router.push({
+      pathname: "/search",
+      query: {
+        location: searchInput,
+        startDate: startDate.toISOString(),
+        endDate: endDate.toISOString(),
+        noOfGuests,
+      },
+    });
+  };
+
   return (
     <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
       {/* Left */}
-      <div className="relative flex items-center h-10 cursor-pointer my-auto">
+      <div
+        onClick={() => router.push("/")}
+        className="relative flex items-center h-10 cursor-pointer my-auto"
+      >
         <Image
           src="https://links.papareact.com/qd3"
           layout="fill" // automatically sets W and H to fill the container
@@ -54,7 +71,7 @@ function Header() {
           onChange={(e) => setSearchInput(e.target.value)}
           type="text"
           className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400"
-          placeholder="Start your search"
+          placeholder={placeholder || "Start your search"}
         />
         <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2" />
       </div>
@@ -100,6 +117,7 @@ function Header() {
               Cancel
             </button>
             <button
+              onClick={search}
               className="flex-grow p-2 text-red-400 
               hover:bg-red-400 hover:text-white hover:rounded-full
               transition-all ease-out duration-150 active:scale-105"
